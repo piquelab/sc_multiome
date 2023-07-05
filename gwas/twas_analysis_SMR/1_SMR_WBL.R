72c7ccdfecc3f31e13e7d476d61c135a3b4218f8
JulongWei <weijulong128@sina.com>
2023-07-05 16:49:46 -0400

    SMR TWAS analysis

--- gwas/twas_analysis_SMR/1_SMR_WBL.R
+++ gwas/twas_analysis_SMR/1_SMR_WBL.R
@@ -6,15 +6,15 @@ rm(list=ls())
 
 
 ### passing argument
-## args=commandArgs(trailingOnly=T)
-## if (length(args)>0){
-##    condition <- args[1]
-##    dir <- args[2]
-##  }else{
-##    condition <- "Bcell_CTRL"
-##    dir <- "SCAIP_results"
-## }
+args=commandArgs(trailingOnly=T)
+if (length(args)>0){
+   trait <- args[1]
+ }else{
+  trait <- "CARDloGRAM_C4D_CAD"
+}
 
+## traits <- read.table("traits_of_interest.txt")$V1
+## traits <- sort(traits)
 
 
 
@@ -23,85 +23,120 @@ rm(list=ls())
 fast <- fread("./eQTL_results/Whole_Blood.fastqtl.gz", header=F, data.table=F) ##9,719,090 SNPs
 names(fast) <- c("gene", "id_b38", "DTSS", "pval", "beta", "se")
 fast <- fast%>%mutate(gene_SNP=paste(gene, id_b38, sep="_"))
-pval_eqtl <- fast$pval
-names(pval_eqtl) <- fast$gene_SNP
+fast <- fast%>%filter(!grepl("chrX", id_b38))
 
+## pval_eqtl <- fast$pval
+## names(pval_eqtl) <- fast$gene_SNP
 
 
 ###
-### 1. PIP results  add grch38 position   
+### fine-mapping,  dap results from multiple condition annotation, default
 fn <- "./eQTL_results/WBL_PIP_allSNPs.txt.gz"
-res <- fread(fn, header=F, data.table=F)
-names(res) <- c("gene", "id_b38", "PIP")
+dap <- fread(fn, header=F, data.table=F)
+names(dap) <- c("gene", "id_b38", "PIP")
+dap <- dap%>%mutate(gene_SNP=paste(gene, id_b38, sep="_")) 
+dap <- dap%>%filter(!grepl("chrX", id_b38))
+PIP <- dap$PIP
+names(PIP) <- dap$gene_SNP
 
 
 ###
-outdir <- "./1_SMR_output/eQTL_conditions/"
+### fine-mapping, dap results from baseline annotation
+fn <- "./eQTL_results/WBL_PIP_allSNPs_base.txt.gz"
+dap2 <- fread(fn, header=F, data.table=F)
+names(dap2) <- c("gene", "id_b38", "PIP")
+dap2 <- dap2%>%mutate(gene_SNP=paste(gene, id_b38, sep="_"))
+dap2 <- dap2%>%filter(!grepl("chrX", id_b38))
+PIP2 <- dap2$PIP
+names(PIP2) <- dap2$gene_SNP
+
+
+
+
+###
+### gwas data
+fn <- paste0("./gwas_data/", trait, "_impute_gwas.txt.gz")
+summ <- fread(fn, header=T, data.table=F)
+summ <- summ%>%dplyr::select(id_b38, chr, pos, zscore_gwas=zscore, pval_gwas=pval)
+
+## pval_gwas <- summ$pval
+## names(pval_gwas) <- as.character(summ$id_b38) ##summ$panel_variant_id
+
+
+###
+### combine fastQTL and fine-mapping together 
+res_comb <- fast%>%dplyr::select(gene, id_b38, gene_SNP, pval_eqtl=pval)%>%
+    mutate(PIP_conditions=PIP[gene_SNP], PIP_base=PIP2[gene_SNP])
+
+
+
+
+###########################################################################
+### TWAS minimum p value to select the reprentative SNPs for each gene ####
+###########################################################################
+
+###
+outdir <- "./1_SMR_output/eQTL_fastQTL/"
 if ( !file.exists(outdir)) dir.create(outdir, showWarnings=F, recursive=T)
 
 
-### SMR analysis 
-traits <- read.table("./traits_ls.txt")$V1
+res_gene <- res_comb%>%group_by(gene)%>%slice_min(order_by=pval_eqtl, n=1)%>%ungroup()%>%as.data.frame() 
+res_gene <- res_gene%>%left_join(summ, by="id_b38")
 
-for (trait in traits){
-##    
-fn <- paste0("./gwas_data/gwas_impute/", trait, "_gwas.txt.gz")
-summ <- fread(fn, header=T, data.table=F)
-names(summ) <- c("id_b38", "chr", "pos", "zscore", "pvalue", "id_b38_2")
-    
-pval <- summ$pvalue
-names(pval) <- as.character(summ$id_b38) ##summ$panel_variant_id
+res2_gene <- res_gene%>%group_by(gene)%>%slice_min(order_by=pval_gwas, n=1, with_ties=F)%>%
+    ungroup()%>%as.data.frame()%>%drop_na(pval_gwas)
+ 
+gfn <- gzfile(paste(outdir, trait, "_minP_twas.txt.gz", sep=""))
+write.table(res2_gene, gfn, row.names=F, col.names=T, quote=F)
 
 
-res$pval_gwas <- pval[as.character(res$id_b38)]
-res2 <- res%>%drop_na(pval_gwas)
 
 
-###
-### high PIP
-res2_gene <- res2%>%group_by(gene)%>%slice_max(order_by=PIP, n=1)%>%ungroup()%>%as.data.frame()
-res2_gene <- res2_gene%>%mutate(gene_SNP=paste(gene, id_b38, sep="_"))
+ 
+#######################################################################
+### TWAS top PIP from to select the reprentative SNPs for each gene ###
+###       integration multiple conditions annotation                ###
+#######################################################################
 
-###
-## if ties, we pick the SNP with minimum p-value
-res2_gene$pval_eqtl <- pval_eqtl[as.character(res2_gene$gene_SNP)]
-res3_gene <- res2_gene%>%group_by(gene)%>%slice_min(order=pval_eqtl, n=1)%>%
-    ungroup()%>%as.data.frame()
+outdir <- "./1_SMR_output/eQTL_dap_conditions/"
+if ( !file.exists(outdir)) dir.create(outdir, showWarnings=F, recursive=T)
+ 
+res_gene <- res_comb%>%drop_na(PIP_conditions)%>%
+    group_by(gene)%>%slice_max(order_by=PIP_conditions, n=1)%>%ungroup()%>%as.data.frame()
+res_gene <- res_gene%>%group_by(gene)%>%slice_min(order_by=pval_eqtl, n=1)%>%ungroup()%>%as.data.frame()
+res_gene <- res_gene%>%left_join(summ, by="id_b38")
 
-### if ties, we pick the SNP with minimum of gwas 
-res3_gene <- res3_gene%>%group_by(gene)%>%
-    slice_min(order_by=pval_gwas, n=1, with_ties=F)%>%ungroup()%>%
-    mutate(gene2=gsub("\\..*", "", gene))%>%
-    as.data.frame() ### 19689 
-
+res2_gene <- res_gene%>%group_by(gene)%>%slice_min(order_by=pval_gwas, n=1, with_ties=F)%>%
+    ungroup()%>%as.data.frame()%>%drop_na(pval_gwas)
+     
 gfn <- gzfile(paste(outdir, trait, "_topPIP_twas.txt.gz", sep=""))
-write.table(res3_gene, gfn, row.names=F, col.names=T, quote=F)
-
-cat(trait, nrow(res3_gene), "\n")
-    
-}    
-### End
+write.table(res2_gene, gfn, row.names=F, col.names=T, quote=F)
 
 
-###
-### correct pvalue, *2
 
-## traits <- read.table("./traits_ls.txt")$V1
-## dap <- c("eQTL_base", "eQTL_conditions", "eQTL_union")
+#######################################################################
+### TWAS top PIP from to select the reprentative SNPs for each gene ###
+### integration baseline fine-mapping results                       ###
+#######################################################################
 
-## for (ii in dap[-1]){
-## ## 
-## for (trait in traits[-1]){
-##    ###
-##    fn <- paste("./1_SMR_output/", ii, "/", trait, "_topPIP_twas.txt.gz", sep="")
-##    summ <- fread(fn, header=T, data.table=F)
-##    summ <- summ%>%mutate(pval_gwas=pval_gwas*2)
-##    ###
-##    gfn  <- gzfile(paste("./1_SMR_output/", ii, "/", trait, "_topPIP_twas.txt.gz", sep=""))
-##    write.table(summ, gfn, row.names=F, col.names=T, quote=F)
-##    cat(ii, trait, "\n")
-## } ### traits
-    
-## } ### dap    
+
+outdir <- "./1_SMR_output/eQTL_dap_base/"
+if ( !file.exists(outdir)) dir.create(outdir, showWarnings=F, recursive=T)
+
+ 
+res_gene <- res_comb%>%drop_na(PIP_base)%>%
+    group_by(gene)%>%slice_max(order_by=PIP_base, n=1)%>%ungroup()%>%as.data.frame()
+res_gene <- res_gene%>%group_by(gene)%>%slice_min(order_by=pval_eqtl, n=1)%>%ungroup()%>%as.data.frame()
+res_gene <- res_gene%>%left_join(summ, by="id_b38")
+
+res2_gene <- res_gene%>%group_by(gene)%>%slice_min(order_by=pval_gwas, n=1, with_ties=F)%>%
+    ungroup()%>%as.data.frame()%>%drop_na(pval_gwas)
+     
+gfn <- gzfile(paste(outdir, trait, "_topPIP_twas.txt.gz", sep=""))
+write.table(res2_gene, gfn, row.names=F, col.names=T, quote=F)
+
+
+
+###END
     
     
---
+++ gwas/twas_analysis_SMR/1_SMR_WBL_submit.sh
@@ -0,0 +1,14 @@
+#!/bin/bash/
+
+
+cd $PWD
+
+cat traits_of_interest.txt | \
+while read trait; do
+   ###
+   sbatch -q express -p erprp --mem=120G --time=3-00:00:00 -N 1-1 -n 1 --job-name=SMR_${trait} --output=slurm_SMR_${trait}.output --wrap "
+   module load R;
+   R CMD BATCH --no-save --no-retore '--args ${trait}' 1_SMR_WBL.R zzz_SMR_${trait}.Rout"
+   echo ${trait}
+   sleep 1;
+done
